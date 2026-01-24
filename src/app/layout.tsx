90d39219a143f19e1904c5115bead6cd0bb8a7b5
Rivu Chattopadhyay <rivu851@gmail.com>
2026-01-25 01:38:09 +0530

    Added About Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { constructMetaData } from '@/utils/functions';
+import { Orbitron } from 'next/font/google';
 
 export const metadata: Metadata = constructMetaData({
   title: 'Techtrix 2026',
   description: 'The Official Sports Fest of RCCIIT.',
+});
+
+const orbitron = Orbitron({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-orbitron',
 });
 
 export default function RootLayout({
@@ -14,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`$antialiased`}>{children}</body>
+      <body className={`${orbitron.className} antialiased`}>{children}</body>
     </html>
   );
 }
