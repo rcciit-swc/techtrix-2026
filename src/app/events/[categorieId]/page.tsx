9e4af08a563168a94f5787abaae958b0c1910e6c
Soumyaraj Bag <--global>
2026-02-12 04:25:06 +0530

    add:improvements

--- src/app/events/[categorieId]/page.tsx
+++ src/app/events/[categorieId]/page.tsx
@@ -1,29 +1,27 @@
 // app/events/[categorieId]/page.tsx
-import ComingSoon from '@/components/ComingSoon';
 
 export default function EventsPage() {
   return (
-    <ComingSoon />
-    // <div
-    //   className="min-h-screen relative overflow-hidden bg-cover bg-center bg-fixed"
-    //   style={{ backgroundImage: "url('/profile/profilebg.jpeg')" }}
-    // >
-    //   {/* Dark Overlay */}
-    //   <div className="absolute inset-0 bg-black/70" />
+    <div
+      className="min-h-screen relative overflow-hidden bg-cover bg-center bg-fixed"
+      style={{ backgroundImage: "url('/profile/profilebg.jpeg')" }}
+    >
+      {/* Dark Overlay */}
+      <div className="absolute inset-0 bg-black/70" />
 
-    //   {/* Animated Background Overlay */}
-    //   <div
-    //     className="absolute inset-0 pointer-events-none"
-    //     style={{
-    //       background:
-    //         'radial-gradient(circle at 50% 50%, rgba(250, 204, 21, 0.1) 0%, transparent 70%)',
-    //       animation: 'pulse 8s infinite',
-    //     }}
-    //   />
+      {/* Animated Background Overlay */}
+      <div
+        className="absolute inset-0 pointer-events-none"
+        style={{
+          background:
+            'radial-gradient(circle at 50% 50%, rgba(250, 204, 21, 0.1) 0%, transparent 70%)',
+          animation: 'pulse 8s infinite',
+        }}
+      />
 
-    //   <main className="relative z-10">
-    //     <EventCardsCluster />
-    //   </main>
-    // </div>
+      <main className="relative z-10">
+        <EventCardsCluster />
+      </main>
+    </div>
   );
 }
